b5df3d849693d319543ce996b75ac87110ca1f31
alfina1515 <alfinaiswahyuni002@gmail.com>
2025-03-25 19:07:50 +0700

    membuat fungsi input elemen array

--- Contoh_InsertionSort.cpp
+++ Contoh_InsertionSort.cpp
@@ -3,3 +3,12 @@ using namespace std;
 
 int arr[20]; // membuat array dengan panjang data 20
 int n; // membuat variable inputan n
+
+void input()
+{ // procedure input
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : "; // membuat inputan jumlah array
+        cin >> n; // memanggil variable inputan n
+    }
+}
