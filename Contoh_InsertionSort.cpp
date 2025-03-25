e6dc10b76cac6cd1cbe8f0d6f83a2480553c789f
alfina1515 <alfinaiswahyuni002@gmail.com>
2025-03-25 19:16:44 +0700

    menambahkan header input array

--- Contoh_InsertionSort.cpp
+++ Contoh_InsertionSort.cpp
@@ -15,8 +15,13 @@ void input()
         { // membuat kondisi n tidak lebih dari 20
             break;
         }
-        else{
+        else
+        {
             cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; //menampilkan pesan jika data lebih dari 20
         }
     }
+cout << endl; // membuat jarak per baris program
+cout << "======================" << endl; // membuat tampilan susunan data element array
+cout << "Masukkan Element Array" << endl;
+cout << "======================" << endl;
 }
